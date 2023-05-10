c88d5d33a450544676ba58e225f1924d373905d0
Novembver212 <vf.november212@gmail.com>
2023-05-10 10:13:21 +0300

    t.web.lab6.2/adding new files

--- project-t.web/src/Menu.tsx
+++ project-t.web/src/Menu.tsx
@@ -1,4 +1,5 @@
 import { Menu } from 'antd';
+import { Link } from 'react-router-dom';
 
 const { SubMenu } = Menu;
 
@@ -6,11 +7,17 @@ function MenuBlock() {
     return (
         <Menu mode="inline">
             <SubMenu key="sub1" title="Menu Item 1">
-                <Menu.Item key="1">Submenu Item 1</Menu.Item>
-                <Menu.Item key="2">Submenu Item 2</Menu.Item>
+                <Menu.Item key="1">
+                    <Link to="/">Login Form</Link>
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <Link to="/card">Card</Link>
+                </Menu.Item>
             </SubMenu>
             <SubMenu key="sub2" title="Menu Item 2">
-                <Menu.Item key="3">Submenu Item 3</Menu.Item>
+                <Menu.Item key="3">
+                    <Link to="/form">Form Block</Link>
+                </Menu.Item>
                 <Menu.Item key="4">Submenu Item 4</Menu.Item>
             </SubMenu>
         </Menu>
