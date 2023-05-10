1ac49620ea7aa35a577b5001ad6a234b750dd421
Novembver212 <vf.november212@gmail.com>
2023-05-10 03:54:21 +0300

    t.web.lab3 is completed

--- project-t.web/src/App.tsx
+++ project-t.web/src/App.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './Menu'
-import Card from './Card'
-import FormBlock from './FormBlock'
+import Menu from './Menu';
+import Card from './Card';
+import FormBlock from './FormBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Menu />
+          <Card />
+          <FormBlock />
+        </header>
+      </div>
   );
 }
 
